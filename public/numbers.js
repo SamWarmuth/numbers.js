aa81676d878678c51c6293da4f0ff9d3e9256cf2
Sam Warmuth <sam@samwarmuth.com>
2012-12-12 11:59:23 -0600

    Update public/numbers.js

--- public/numbers.js
+++ public/numbers.js
@@ -47,7 +47,6 @@ require.resolve = (function () {
             if (require.modules[x]) {
                 return x;
             }
-            
             for (var i = 0; i < require.extensions.length; i++) {
                 var ext = require.extensions[i];
                 if (require.modules[x + ext]) return x + ext;
